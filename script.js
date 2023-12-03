5996ff59123643f101e5d7a26d6fc2025a5de10c
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-12-03 10:17:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,38 +3,39 @@
 function download() {
     const apiUrl = 'https://abobus-snowy.vercel.app/api/download';
     const videoUrl = document.getElementById('videoUrl').value;
-  
+
     if (!videoUrl) {
-      alert('Por favor, insira a URL do vídeo.');
-      return;
+        alert('Por favor, insira a URL do vídeo.');
+        return;
     }
-  
-    fetch('/download', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `videoUrl=${encodeURIComponent(videoUrl)}`,
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `videoUrl=${encodeURIComponent(videoUrl)}`,
     })
     .then(response => {
-      if (!response.ok) {
-        throw new Error('Erro ao iniciar o download.');
-      }
-      return response.blob();
+        if (!response.ok) {
+            throw new Error('Erro ao iniciar o download.');
+        }
+        return response.blob();
     })
     .then(blob => {
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'audio.mp3';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'audio.mp3';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
     })
     .catch(error => {
-      console.error(error);
-      alert('Erro ao processar o download.');
+        console.error(error);
+        alert('Erro ao processar o download.');
     });
-  }
+}
+
   
