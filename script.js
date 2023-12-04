f7a3806aa5e10457ace65b4bd8ea156c0279929a
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-12-03 21:08:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,15 @@ function download() {
         method: 'POST',
         mode: 'cors',
         credentials: 'include',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ];
+  },
         body: `videoUrl=${encodeURIComponent(videoUrl)}`,
     })
     .then(response => {
